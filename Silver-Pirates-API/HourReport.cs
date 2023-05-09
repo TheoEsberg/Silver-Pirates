ef76c3259db2f348cb5148ad1f4180e07f637adf
Theo Esberg <esbergtheo@gmail.com>
2023-05-09 17:34:28 +0200

    Removed unused Using statements from the classes

--- Silver-Pirates-API/HourReport.cs
+++ Silver-Pirates-API/HourReport.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Silver_Pirates_API
 {
@@ -16,3 +11,6 @@ namespace Silver_Pirates_API
         public Employee Employee { get; set; }
     }
 }
+
+
+
